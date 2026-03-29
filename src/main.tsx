8d2c5b976de6260c0e493ca1994dfb4d9246e581
mpmariapaula <mariapaula.cnpq@gmail.com>
2026-03-28 22:33:00 -0300

    ajuste de paginas e categorias

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './app/App' // ajuste se necessário
+import './styles/index.css' // ajuste se necessário
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
